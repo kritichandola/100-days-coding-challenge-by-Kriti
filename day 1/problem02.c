16bca57820c5b7a9b5f49f558b3388c35edbe53a
Kriti Chandola <kritichandola302@gmail.com>
2026-06-16 16:26:39 +0530

    Day 1 completed

---
+++ day 1/problem02.c
@@ -0,0 +1,26 @@
+/*Find the sum of all array elements using an optimized approach.*/
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    int actualSum = 0;
+
+    printf("Enter the elements:\n");
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        actualSum += arr[i];
+    }
+
+    int expectedSum = n * (n + 1) / 2;
+    int missing = expectedSum - actualSum;
+
+    printf("Missing number is %d", missing);
+
+    return 0;
+}
