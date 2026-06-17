335b73d6d230bbbde0b5a5055f073985b538bff0
Kriti Chandola <kritichandola302@gmail.com>
2026-06-17 22:25:50 +0530

    Day 3 solutions

---
+++ day 3/problem01.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n, target, count = 0;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter target: ");
+    scanf("%d", &target);
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == target) {
+            count++;
+        }
+    }
+
+    printf("Number of occurrences = %d", count);
+
+    return 0;
+}
